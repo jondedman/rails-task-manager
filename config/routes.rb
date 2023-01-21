caf374900a40d18bfe6216b06643a57a53b7a02c
Jon Dedman <jonathandedman@googlemail.com>
2023-01-21 16:19:18 +0000

    implement todo list and refactor. still need to design front end

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,22 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # root 'tasks#index'
+  # get "tasks/new", to: "tasks#new",  as: :new_task
+  # get "tasks/:id", to: "tasks#show", as: :task
+  root 'tasks#index'
+resources :tasks
 end
+
+#  # Read all
+#  get    "restaurants",          to: "restaurants#index"
+#  # Create
+#  get    "restaurants/new",      to: "restaurants#new",  as: :new_restaurant
+#  post   "restaurants",          to: "restaurants#create"
+#  # Read one - The `show` route needs to be *after* `new` route.
+#  get    "restaurants/:id",      to: "restaurants#show", as: :restaurant
+#  # Update
+#  get    "restaurants/:id/edit", to: "restaurants#edit", as: :edit_restaurant
+#  patch  "restaurants/:id",      to: "restaurants#update"
+#  # Delete
+#  delete "restaurants/:id",      to: "restaurants#destroy"
